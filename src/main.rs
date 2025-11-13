61653b47189369be1696cdc9615660948c2086c9
gbart0198 <gbart0198@gmail.com>
2025-11-12 22:59:09 -0500

    use -f and -d to specify file pattern or directory, respectively

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,38 @@ use std::{fs, io};
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// The directory to search, e.g '.' for current directory, './app' for app directory
-    directory_name: String,
-
     /// The pattern to search against
     pattern: String,
+
+    /// The directory to search, e.g '.' for current directory, './app' for app directory
+    #[arg(short, long, default_value = ".")]
+    directory: String,
+
+    /// The file pattern to search against
+    #[arg(short, long, default_value = "*")]
+    file: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
+    let mut total = 0;
 
-    for entry in fs::read_dir(args.directory_name)? {
+    for entry in fs::read_dir(args.directory)? {
         let entry = entry?;
         let path = entry.path();
         if path.is_file() {
-            let _count = grep_file(path.as_path(), &args.pattern);
+            let file_name = path.to_string_lossy();
+            if args.file != "*" {
+                if file_name.contains(&args.file) {
+                    total += grep_file(&path, &args.pattern);
+                }
+            } else {
+                total += grep_file(&path, &args.pattern);
+            }
         }
     }
+
+    println!("Total matches found: {}", total);
 
     Ok(())
 }
